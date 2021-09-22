6abe2332dedaef5c7883b333341e3700459b1ef0
CarterWurst2023 <816568@mystma.org>
2021-09-22 09:10:55 -0500

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(a,b,c,d,e):
-    return a+b+c+d+e
+def topFiveMovies(a, b,  c, d, e):
+    return a + " "+ b +" " +c + " " +d + " " +e
 
-print(topFiveMovies("spooderman 1 ", "Gardians of the Galaxy 1 ", "Gardians Of The Galaxy 2 ", "Shrek 7 ", "Cars 1 "))
+
